8ce99b2d867373688005432be3fa79fa4540fd27
Bryan Johns <73319490+johbry17@users.noreply.github.com>
2024-02-01 20:19:48 -0500

    v0.0 added a donut chart, table formatting

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 from dash import Dash, dcc, html, dash_table, callback, Input, Output, State, ctx
 import dash
 from dash.dash_table import FormatTemplate
-import pandas as pd
+import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
+import pandas as pd
+import re
 
 
 def main():
-    external_stylesheets = [dbc.themes.BOOTSTRAP, "/assets/style.css"]
-
-    app = Dash(external_stylesheets=external_stylesheets)
+    app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
     # layout of html page
     app.layout = html.Div(
@@ -67,28 +67,38 @@ def main():
                 ],
             ),
             # budget table
-            dbc.Row(
-                dbc.Col(
-                    dash_table.DataTable(
-                        id="budget-table",
-                        editable=True,
-                        row_deletable=True,
-                        style_table={
-                            "overflowX": "auto"
-                        },  # enable horizontal scrolling
-                        style_cell={
-                            "whiteSpace": "normal",
-                            "height": "auto",
-                        },  # enable word wrap
-                        style_data_conditional=[
-                            {"if": {"column_id": "Expense"}, "textAlign": "right"},
-                            {"if": {"column_id": "Price"}, "textAlign": "center"},
-                            {"if": {"column_id": "Notes"}, "textAlign": "left"},
-                        ],
+            dbc.Card(
+                dbc.CardBody(
+                    dbc.Row(
+                        dbc.Col(
+                            dash_table.DataTable(
+                                id="budget-table",
+                                editable=True,
+                                row_deletable=True,
+                                style_table={
+                                    "overflowX": "auto"
+                                },  # enable horizontal scrolling
+                                style_cell={
+                                    "whiteSpace": "normal",
+                                    "height": "auto",# enable word wrap
+                                    "border": "2px solid #888",
+                                },
+                                style_data_conditional=[
+                                    {"if": {"column_id": "Expense"}, "textAlign": "right"},
+                                    {"if": {"column_id": "Price"}, "textAlign": "center"},
+                                    {"if": {"column_id": "Notes"}, "textAlign": "left"},
+                                    {
+                                        "if": {"row_index": "odd"},
+                                        "backgroundColor": "lightgrey"
+                                    },
+                                ],
+                            ),
+                            width=6,
+                            className="mx-auto",
+                        )
                     ),
-                    width=6,
-                    className="mx-auto",
-                )
+                ),
+                className="mb-3",
             ),
             # buttons to add and delete rows
             html.Div(
@@ -103,36 +113,46 @@ def main():
             html.Br(),
             html.Br(),
             # summary table
-            dbc.Row(
-                dbc.Col(
-                    dash_table.DataTable(
-                        id="summary-table",
-                        columns=[
-                            {"name": "Expense", "id": "Expense"},
-                            {
-                                "name": "Price",
-                                "id": "Price",
-                                "type": "numeric",
-                                "format": FormatTemplate.money(2),
-                            },
-                            {"name": "Notes", "id": "Notes"},
-                        ],
-                        style_table={
-                            "overflowX": "auto"
-                        },  # enable horizontal scrolling
-                        style_cell={
-                            "whiteSpace": "normal",
-                            "height": "auto",
-                        },  # enable word wrap
-                        style_data_conditional=[
-                            {"if": {"column_id": "Expense"}, "textAlign": "right"},
-                            {"if": {"column_id": "Price"}, "textAlign": "center"},
-                            {"if": {"column_id": "Notes"}, "textAlign": "left"},
-                        ],
+            dbc.Card(
+                dbc.CardBody(
+                    dbc.Row(
+                        dbc.Col(
+                            dash_table.DataTable(
+                                id="summary-table",
+                                columns=[
+                                    {"name": "Expense", "id": "Expense"},
+                                    {
+                                        "name": "Price",
+                                        "id": "Price",
+                                        "type": "numeric",
+                                        "format": FormatTemplate.money(2),
+                                    },
+                                    {"name": "Notes", "id": "Notes"},
+                                ],
+                                style_table={
+                                    "overflowX": "auto"
+                                },  # enable horizontal scrolling
+                                style_cell={
+                                    "whiteSpace": "normal",
+                                    "height": "auto", # enable word wrap
+                                    "border": "2px solid #888",
+                                },
+                                style_data_conditional=[
+                                    {"if": {"column_id": "Expense"}, "textAlign": "right"},
+                                    {"if": {"column_id": "Price"}, "textAlign": "center"},
+                                    {"if": {"column_id": "Notes"}, "textAlign": "left"},
+                                    {
+                                        "if": {"row_index": 1},
+                                        "backgroundColor": "lightgrey"
+                                    },
+                                ],
+                            ),
+                            width=6,
+                            className="mx-auto",
+                        )
                     ),
-                    width=6,
-                    className="mx-auto",
-                )
+                ),
+                className="mb-3",
             ),
             # save button
             html.Div(
@@ -143,6 +163,10 @@ def main():
             ),
             # download link
             dcc.Download(id="download-link"),
+            html.Br(),
+            html.Br(),
+            # donut chart of expenses
+            dcc.Graph(id="expense-donut-chart"),
         ]
     )
 
@@ -182,7 +206,6 @@ def main():
         radio_value,
         derived_virtual_data,
         button_chosen,
-        # hidden_radio_store_data,
         current_data,
         is_modal_open,
     ):
@@ -210,11 +233,12 @@ def main():
                 False,
             )
 
-        # when the modal Cancel button is clicked, close the modal
+        # when the modal Cancel button is clicked, do nothing and close the modal
         elif triggered_button == "modal-cancel-button" and is_modal_open:
             return dash.no_update, dash.no_update, False
 
         # else convert the current_data to a DataFrame, with options to add or delete rows
+        # & autoformat the Price column, using derived_virtual_data (realtime data changes to table)
         else:
             budget = pd.DataFrame(current_data)
 
@@ -253,7 +277,6 @@ def main():
         # for initial load, do nothing
         # if 'Price' column does not exist, return the empty df
         if "Price" not in df.columns:
-            # return df.to_dict('records')
             return dash.no_update
 
         # strips all non-digit characters from the Price column, and converts to numeric - for maths!
@@ -297,12 +320,6 @@ def main():
             # append summary_df to budget_df
             df = pd.concat([budget_df, summary_df], ignore_index=True)
 
-            # reformat Price column, in case of user chicanery in the budget table
-            df.Price = pd.to_numeric(
-                df.Price.replace(r"[^\d.]", "", regex=True), errors="coerce"
-            )
-            df.Price = df.Price.astype(float).map("${:,.2f}".format)
-
             # convert combined dataframe to csv file
             csv_file = df.to_csv(index=False, encoding="utf-8")
 
@@ -312,7 +329,73 @@ def main():
             )
 
             return download_link
+        
+    # callback to update donut chart
+    @app.callback(
+        Output("expense-donut-chart", "figure"),
+        Input("budget-table", "data"),
+        Input("summary-table", "data"),
+        prevent_initial_call=True,
+    )
+    def update_donut_chart(budget_data, summary_data):
+        # if budget_data is empty (initial load), return an empty figure
+        if not budget_data:
+            return go.Figure()
 
+        # extract labels and values from the budget data
+        labels = [row["Expense"] for row in budget_data]
+        values = [row["Price"] for row in budget_data]
+
+        # strip all non-digit chars from values, and convert to float
+        values = [float(re.sub(r"[^\d.]", "", value)) for value in values]
+
+        # Add the buffer from the summary data
+        labels.append("Now 23.1% Buffer")
+        buffer_value = next((row["Price"] for row in summary_data if row["Expense"] == "30% Buffer"), 0)
+        values.append(buffer_value)
+
+        # Calculate the total amount
+        total = sum(values)
+
+        # Calculate the percentages
+        percentages = [value / total for value in values] if total != 0 else values
+
+        title=f"Breakdown of Expenses<br>${total:.2f} Total"
+
+        # Create the figure
+        figure = go.Figure(
+            data=[
+                go.Pie(
+                    labels=labels,
+                    values=percentages,
+                    text=values,
+                    hole=.5,
+                    hovertemplate="%{label}:  $%{text:.2f}<extra></extra>",
+                    textinfo="percent",
+                )
+            ],
+            layout=go.Layout(
+                title="Breakdown of Expenses",
+                showlegend=True,
+                annotations=[
+                    dict(
+                        x=0.5,
+                        y=-0.1,
+                        showarrow=False,
+                        text=f"Total: ${total:.2f}",
+                        xref="paper",
+                        yref="paper",
+                        font=dict(
+                            size=20,
+                        )
+                    )
+                ]
+            ),
+        )
+
+        return figure
+
+    # initialize the app
     if __name__ == "__main__":
         app.run(debug=True)
 
